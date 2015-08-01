41495cbb1be6b0c85b915eb5f681228a4fb54a83
Sergey Kukunin <sergey.kukunin@gmail.com>
2015-08-02 00:50:09 +0300

    Configure flx-ido for better autocomplete

--- init/55-autocomplete.el
+++ init/55-autocomplete.el
@@ -4,4 +4,14 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
+;;; Better ido
+(require-package 'flx-ido)
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
 (provide '55-autocomplete)
