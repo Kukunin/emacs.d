420e9a241c336a4aabae7407360f7a9097501d40
Sergey Kukunin <sergey.kukunin@gmail.com>
2016-02-24 11:48:05 +0200

    Set autosave on focus lost

--- init/15-ui.el
+++ init/15-ui.el
@@ -9,4 +9,10 @@
 ;; Expand region package
 (require-package 'expand-region)
 
+;; Autosave
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+(add-hook 'focus-out-hook 'save-all)
+
 (provide '15-ui)
